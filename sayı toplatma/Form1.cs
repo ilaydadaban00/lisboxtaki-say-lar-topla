529dabd25e439a43fcf8043e1f2b5ea3b782b995
ilaydadaban00 <145451273+ilaydadaban00@users.noreply.github.com>
2023-11-27 04:13:27 +0300

    Add files via upload

---
+++ sayı toplatma/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace sayı_toplatma
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Random rnd = new Random();
+            int sayac = 0;
+            for(int i= sayac; i < 20;i++)
+            {
+                
+                listBox1.Items.Add(i);
+                sayac++;
+                MessageBox.Show(sayac.ToString());
+            }
+        }
+    }
+}
